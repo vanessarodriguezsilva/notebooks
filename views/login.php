21dcdcd3a942fb0cec7fa3095e54b3aeb7a3e5a5
Moises Ernesto Pachon Bustos <Moises.Pachon@mullenlowessp3.com>
2018-07-24 08:35:59 -0500

    fix link include header and footer in views

--- views/login.php
+++ views/login.php
@@ -1,11 +1,8 @@
-<?php include('core/header.php'); ?>
+<?php include('../core/header.php'); ?>
 
 <!-- AQUI VA LOGIN -->
 
 <P>AQUI VA EL LOGIN</P>
 
 
-
-
-
-<?php include('core/footer.php'); ?>
+<?php include('../core/footer.php'); ?>

68d019d433b93d37bd4d60cd332034f72c178253
Moises Ernesto Pachon Bustos <Moises.Pachon@mullenlowessp3.com>
2018-07-25 08:29:04 -0500

    Structure of login and register

--- views/login.php
+++ views/login.php
@@ -1,8 +1,24 @@
 <?php include('../core/header.php'); ?>
 
-<div id="login">
-    <!-- AQUI VA LOGIN -->
-    <P>AQUI VA EL LOGIN</P>
+<!-- AQUI VA LOGIN -->
+
+<P>AQUI VA EL LOGIN</P>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-6">
+            <div class="log-head">
+                <div class="row">
+                    <div class="col-xs-6">
+                        <a href="#">Log In</a>
+                    </div>
+                    <div class="col-xs-6">
+                        <a href="#">Register</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
 
 <?php include('../core/footer.php'); ?>

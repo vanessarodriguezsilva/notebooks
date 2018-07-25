6eb672fa5c1ba40a21e9c86c8d7ed463e8ca328f
Moises Ernesto Pachon Bustos <Moises.Pachon@mullenlowessp3.com>
2018-07-25 09:07:48 -0500

    Create forms login and register

--- views/login.php
+++ views/login.php
@@ -9,11 +9,29 @@
             <div class="log-head">
                 <div class="row">
                     <div class="col-xs-6">
-                        <a href="#">Log In</a>
+                        <a href="#">Iniciar Sesión</a>
                     </div>
                     <div class="col-xs-6">
-                        <a href="#">Register</a>
+                        <a href="#">Registrarse</a>
                     </div>
+                </div>
+                <div class="log-body">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <form action="#" method="post">
+                                <input type="text" name="user" placeholder="Usuario" required>
+                                <input type="password" name="pass" placeholder="Contraseña" required>
+                                <input type="submit" value="Ingresar">
+                            </form>
+                            <form action="#" method="post">
+                                <input type="text"  name="user" placeholder="Usuario" required>
+                                <input type="email" name="email"placeholder="Correo Electronico" required>
+                                <input type="password" name="pass" placeholder="Contraseña" required>
+                                <input type="password" name="repeatpass" placeholder="Confirmar Contraseña" required>
+                                <input type="submit" value="Crear Cuenta">
+                            </form>
+                        </div>
+                    </div>    
                 </div>
             </div>
         </div>
